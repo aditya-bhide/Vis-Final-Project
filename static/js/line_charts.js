2b3aab5fc00c7f80430b0094160f004a80706960
aditya-bhide <adityabhide1@gmail.com>
2021-05-13 05:45:01 -0400

    commit at 6am

--- static/js/line_charts.js
+++ static/js/line_charts.js
@@ -3,7 +3,7 @@
 function multi_line_chart(data) {
     var svg = d3.select("#line-chart-svg")
         .append('svg')
-        .attr("width", 600)
+        .attr("width", 650)
         .attr("height", 390);
     ticks_values = [1979, 1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
     var svg = d3.select("#line-chart-svg svg"),
